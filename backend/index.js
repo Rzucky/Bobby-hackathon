ca5edbb0b13692b2e55d5e392bd48346b9650375
Rzucky <roko.mihalic@gmail.com>
2023-11-04 12:44:11 +0100

    Prisma start

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,10 @@ const {createServer} = require('http');
 const {Server} = require('socket.io');
 const express = require('express');
 
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
+
 const cors = require("cors");
 /*
     Explanation:
@@ -14,40 +18,40 @@ const connectionString = "Endpoint=sb://cbq-hackathon.servicebus.windows.net/;Sh
 const eventHubName = "team7";
 const consumerGroup = "$Default";
 
-const consumerClient = new EventHubConsumerClient(consumerGroup, connectionString, eventHubName);
+// const consumerClient = new EventHubConsumerClient(consumerGroup, connectionString, eventHubName);
 
-const subscription = consumerClient.subscribe({
+// const subscription = consumerClient.subscribe({
 
-        /*
-            Explanation:
-            - processEvents is called whenever the consumer client receives events.
-            - it will always receive a batch of events, even if there is only 1 event in the batch.
-            - events will always come in order they were sent to Event Hub by the producer(simulation).
-            - at end of processEvents, you need to update the checkpoint/offset to the latest event (to prevent processing past events).
-         */
-        processEvents: async (events, context) => {
-            console.log(`Received events: ${events.length}`);
+//         /*
+//             Explanation:
+//             - processEvents is called whenever the consumer client receives events.
+//             - it will always receive a batch of events, even if there is only 1 event in the batch.
+//             - events will always come in order they were sent to Event Hub by the producer(simulation).
+//             - at end of processEvents, you need to update the checkpoint/offset to the latest event (to prevent processing past events).
+//          */
+//         processEvents: async (events, context) => {
+//             console.log(`Received events: ${events.length}`);
 
-            if (events.length === 0) {
-                console.log(`No events received within wait time. Waiting for next interval`);
-                return;
-            }
+//             if (events.length === 0) {
+//                 console.log(`No events received within wait time. Waiting for next interval`);
+//                 return;
+//             }
 
-            for (const event of events) {
-                console.table(event.body)
+//             for (const event of events) {
+//                 console.table(event.body)
 
-                io.emit('ps', event.body);
-            }
+//                 io.emit('ps', event.body);
+//             }
 
-            await context.updateCheckpoint(events[events.length - 1]);
-        },
+//             await context.updateCheckpoint(events[events.length - 1]);
+//         },
 
-        processError: async (err, context) => {
-            console.log(`Error : ${err}`);
-        }
-    },
-    {startPosition: latestEventPosition}
-);
+//         processError: async (err, context) => {
+//             console.log(`Error : ${err}`);
+//         }
+//     },
+//     {startPosition: latestEventPosition}
+// );
 
 console.log("subscription setup done", subscription.isRunning);
 // Import express
@@ -69,6 +73,25 @@ app.get('/', (req, res) => {
     // Send 'Hello World' response
     res.send('Hello World');
 });
+
+
+app.get('/test_insert', async (req, res)=> {
+    const user = await prisma.user.create({
+      data: {
+        type: 'user',
+        name: 'Bobby',
+        email: 'bobby@tablice.drop'
+      },
+    })
+    console.log(user)
+    try {
+      await prisma.$disconnect()
+    } catch (error) {
+      console.error(e)
+      await prisma.$disconnect()
+      process.exit(1)
+    }
+  })
 
 io.on('connection', (socket) => {
     console.log('a user connected');
